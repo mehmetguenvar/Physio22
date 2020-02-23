45d18dbe40ed67d54f8a1ebb3e220c9706783067
FTS <FTS@DESKTOP-BNM88F2>
2020-02-23 08:12:07 +0100

    request to get uebung and kund added

--- app/src/main/java/com/example/mehme/physio22/webservices/IPhysioService.java
+++ app/src/main/java/com/example/mehme/physio22/webservices/IPhysioService.java
@@ -1,14 +1,19 @@
 package com.example.mehme.physio22.webservices;
 
+import com.example.mehme.physio22.dtos.KundenDatenDTO;
 import com.example.mehme.physio22.dtos.TokenDTO;
 import com.example.mehme.physio22.dtos.UebungDTO;
 import com.example.mehme.physio22.dtos.UserLoginDTO;
 
+import java.util.LinkedList;
 import java.util.List;
 
+import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface IPhysioService {
@@ -17,8 +22,14 @@ public interface IPhysioService {
     Call<List<UebungDTO>> listRepos(String authentication);
 
     @POST("/api/authenticate")
-    Call<TokenDTO> authenticate(@Body UserLoginDTO userLoginDTO);
+    Observable<ResponseAuthentication> authenticate(@Body UserLoginDTO userLoginDTO);
 
+    //@Headers("Accept: application/json", "Content-Type: application/json")
+    @GET("/api/uebungs")
+    Observable<List<UebungDTO>> getAllUebung(@Header("Authorization")ResponseAuthentication responseAuthentication);
+
+    @GET("/api/kunden-datens")
+    Observable<LinkedList<KundenDatenDTO>> getAllKunden(@Header("Authorization")ResponseAuthentication responseAuthentication);
     // REST requests
 
     // z.b Post für Übung hochladen
